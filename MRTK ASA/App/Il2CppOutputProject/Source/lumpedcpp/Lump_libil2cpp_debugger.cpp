907f7ea63d1cdbe0048073310a0d2ef9da6948fb
Arunkumar70 <58801830+Arunkumar70@users.noreply.github.com>
2020-04-13 14:39:26 +0530

    1st trial app build

---
+++ MRTK ASA/App/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
@@ -0,0 +1,2 @@
+#include "il2cpp-config.h"
+#include "C:\Users\Veeruby Technology\Documents\GitHub\Microsoft-Android-ioS-ASA-Tutorial\MRTK ASA\App\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
