202fe752eaec45fb575b005d6ed9aa6b52c1f473
Arunkumar70 <58801830+Arunkumar70@users.noreply.github.com>
2020-04-20 08:38:18 +0530

    Deleted external android sdk related problems

--- MRTK ASA/App/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Veeruby Technology\Documents\GitHub\Microsoft-Android-ioS-ASA-Tutorial\MRTK ASA\App\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\Veeruby Technology\Documents\GitHub\Microsoft-Android-ioS-ASA-Tutorial\MRTK ASA\App\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
